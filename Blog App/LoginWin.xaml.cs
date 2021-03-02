6102f8e33693ed0cbe137b2f47ca661931d8c45a
Artur Shabunov <artixd@yandex.ru>
2021-03-02 12:18:05 +0200

    First push

---
+++ Blog App/LoginWin.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Blog_App
+{
+    /// <summary>
+    /// Interaction logic for LoginWin.xaml
+    /// </summary>
+    public partial class LoginWin : Window
+    {
+        MainWindow mainWindow = new MainWindow();
+        public LoginWin(MainWindow obj)
+        {
+            InitializeComponent();
+            mainWindow = obj;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if(username.Text == "test" && password.Password == "test")
+            {
+                
+                mainWindow.IsEnabled = true;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Wrong password or username", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ Blog App/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Data.Entity.Core.Objects;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Blog_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DfLessonDbEntities dataEntities = new DfLessonDbEntities(); // MSSQL database
+        public MainWindow()
+        {
+            InitializeComponent();
+            string[] testTables = new string[] {"Test","Hello","YAAY" };
+            foreach (var x in testTables)
+            {
+                ListBoxTables.Items.Add(x);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //LoginWin loginWin = new LoginWin(this); // where 'this' is reference of current window
+            //loginWin.Show();
+        }
+    }
+}
